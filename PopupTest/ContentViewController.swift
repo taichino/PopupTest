cbae68706ccbfa75c7acb14d333c061fe6e4f3a9
Andrej Mihajlov <and@codeispoetry.ru>
2017-02-15 12:59:55 +0000

    Upgrade to Swift 3.0

--- PopupTest/ContentViewController.swift
+++ PopupTest/ContentViewController.swift
@@ -5,35 +5,35 @@ class ContentViewController : NSViewController {
     var searchField : NSSearchField!
     
     deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NotificationCenter.default.removeObserver(self)
     }
     
     override func loadView() {
         view = NSView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.addConstraint(NSLayoutConstraint(
-            item: view, attribute: .Width, relatedBy: .Equal,
-            toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 280))
+            item: view, attribute: .width, relatedBy: .equal,
+            toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 280))
         view.addConstraint(NSLayoutConstraint(
-            item: view, attribute: .Height, relatedBy: .Equal,
-            toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
+            item: view, attribute: .height, relatedBy: .equal,
+            toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 200))
         
         searchField = NSSearchField()
         searchField.translatesAutoresizingMaskIntoConstraints = false
-        searchField.focusRingType = .None
+        searchField.focusRingType = .none
         view.addSubview(searchField)
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-(20)-[searchField]-(20)-|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["searchField":searchField]))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-(20)-[searchField(==30)]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["searchField":searchField]))
+        view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-(20)-[searchField]-(20)-|", options: [], metrics: nil, views: ["searchField":searchField]))
+        view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-(20)-[searchField(==30)]", options: [], metrics: nil, views: ["searchField":searchField]))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onTextChange:", name: NSControlTextDidChangeNotification, object: searchField)
+        NotificationCenter.default.addObserver(self, selector: #selector(ContentViewController.onTextChange(_:)), name: NSNotification.Name.NSControlTextDidChange, object: searchField)
     }
     
-    func onTextChange(note : NSNotification) {
+    func onTextChange(_ note : Notification) {
         NSLog("Search for %@", searchField.stringValue)
     }
 }
